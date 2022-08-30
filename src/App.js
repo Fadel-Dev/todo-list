3596f9955e7d606f8be8c6379a19b60061f62a66
fadel diop <diopfadel06@gmail.com>
2022-08-30 16:16:06 +0000

    before adding style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Header from './Header';
 
 function App() {
+
+const [todolist, setTodolist]= useState([]);
+const [newTask, setNewTask]= useState("");
+
+const handlerChange=(event)=>{
+  setNewTask(event.target.value);
+};
+const addTask = ()=>{
+  const task={
+    id:todolist.length===0 ? 1 :todolist[todolist.length-1].id+1,
+    taskName:newTask,
+  }
+  setTodolist([...todolist,task]);
+}
+ /*const deleteTask = (taskName)=>{
+ const newTodoList= todolist.filter((task)=>{
+ if(task===taskName){
+    return false;
+  }else{
+    return true;
+  }
+ })
+  setTodolist(newTodoList);
+}
+*/
+const deleteTask = (id)=>{
+ 
+  setTodolist( todolist.filter((task)=> task.id !==id));
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{position:"relative",margin:0}}>
+      < Header />
+      <div className='addTask'>
+        <input onChange={handlerChange}/>
+        <button onClick={addTask} >Add Task</button>
+      </div>
+      <div className='list'>
+        {
+          todolist.map((task)=>{
+           return <h1>{task.taskName} <button onClick={()=>deleteTask(task.id)}>x</button> </h1>
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    const header={
+        position:"abolute",
+        top:0,
+        backgroundColor:"red",
+    }
+  return (
+    <div style={header}>
+        <h1>CoolMan</h1>
+    </div>
+  )
+}
